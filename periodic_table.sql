759ea6e3c1140c0ffb0323d3982b50064bfa646a
nicholaslpollard <nicholaslpollard@gmail.com>
2025-04-16 09:15:13 -0400

    fix: correct output format

--- periodic_table.sql
+++ periodic_table.sql
@@ -16,6 +16,7 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+
 DROP DATABASE periodic_table;
 --
 -- Name: periodic_table; Type: DATABASE; Schema: -; Owner: postgres
